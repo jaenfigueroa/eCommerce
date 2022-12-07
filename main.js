22987abe66980609357eee13af02257335c7ab82
jaen <jaenfigueroa03042000@gmail.com>
2022-12-07 10:10:01 -0500

    footer adaptable

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 const botonIrCarrito = document.getElementById('boton-ir-carrito')
+const botonVolverMenu = document.getElementById('boton-volver-menu')
+
+const seccion1 = document.getElementById('seccion-1')
+const seccion2 = document.getElementById('seccion-2')
+/////////////////////////////////////////
 
 botonIrCarrito.addEventListener('click', () => {
-  alert('vaac')
+  seccion1.style.display = 'none'
+  seccion2.style.display = 'initial'
+})
+
+botonVolverMenu.addEventListener('click', () => {
+  seccion1.style.display = 'initial'
+  seccion2.style.display = 'none'
 })
